313e138d761bb4da3d2e73366744d9d7badf12ed
Austin Liu <aliu139@users.noreply.github.com>
2015-09-18 23:15:59 -0400

    Inverted Colors

--- src/hello_world.c
+++ src/hello_world.c
@@ -9,20 +9,23 @@ static BitmapLayer *s_bitmap_layer;
 void handle_init(void) {
   //GBitmap for the Arrow
   s_example_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ARROW_TRANSPARENT_BLACK);
-  s_bitmap_layer = bitmap_layer_create(GRect(7, 7, 132, 132));
+  s_bitmap_layer = bitmap_layer_create(GRect(2, 7, 137, 142));
+  
   bitmap_layer_set_bitmap(s_bitmap_layer, s_example_bitmap);
+  
   
 	// Create a window and text layer
 	window = window_create();
-	//text_layer = text_layer_create(GRect(0, 0, 144, 154));
+	text_layer = text_layer_create(GRect(0, 0, 160, 160));
 	
 	// Set the text, font, and text alignment
 	//text_layer_set_text(text_layer, "Hi, I'm a Pebble!");
 	//text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
 	//text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	
+	text_layer_set_background_color(text_layer, GColorBlack);
+  
 	// Add the text layer to the window
-	//layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
 
   // Add arrow image
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bitmap_layer));
@@ -42,7 +45,7 @@ void handle_deinit(void) {
 
   // Destroy the text layer
 	text_layer_destroy(text_layer);
-	
+  
 	// Destroy the window
 	window_destroy(window);
 }
