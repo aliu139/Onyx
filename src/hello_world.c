45eafae81c768d87a372e0e65c9f15126b3a70bd
Matt Li <matthew.yj.li@gmail.com>
2015-09-18 23:53:03 -0400

    Got arrow :D

--- src/hello_world.c
+++ src/hello_world.c
@@ -6,6 +6,7 @@
 
 #include <pebble.h>
 #define ARROW_MARGIN 15
+#define ARROW_IN 10
 
 static Window *s_main_window;
 static Layer *s_canvas_layer;
@@ -25,26 +26,25 @@ static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
   graphics_fill_circle(ctx, center, s_radius-5);
 
   // Draw the 'arrow'
-  int angle = 40;
-  int lAngle = angle - 30;
-  int rAngle = angle + 30;
+  int32_t angle = 0;
+  int32_t lAngle = angle - 40;
+  int32_t rAngle = angle + 40;
 
     // Plot hands
   GPoint c_point = (GPoint) {
-    .x = (int16_t)(sin_lookup(angle) * (int32_t)(s_radius - ARROW_MARGIN) / 360) + center.x,
-    .y = (int16_t)(-cos_lookup(angle) * (int32_t)(s_radius - ARROW_MARGIN) / 360) + center.y,
+    .x = (int16_t)(sin_lookup(TRIG_MAX_ANGLE * angle/360) * (int32_t)(s_radius - ARROW_MARGIN) / TRIG_MAX_ANGLE) + center.x,
+    .y = (int16_t)(-cos_lookup(TRIG_MAX_ANGLE * angle/360) * (int32_t)(s_radius - ARROW_MARGIN) / TRIG_MAX_ANGLE) + center.y,
   };
 
     GPoint l_point = (GPoint) {
-    .x = (int16_t)(sin_lookup(lAngle) * (int32_t)(s_radius - ARROW_MARGIN) / 360) + center.x,
-    .y = (int16_t)(-cos_lookup(lAngle) * (int32_t)(s_radius - ARROW_MARGIN) / 360) + center.y,
+    .x = (int16_t)(sin_lookup(TRIG_MAX_ANGLE * lAngle/360) * (int32_t)(s_radius - ARROW_MARGIN - ARROW_IN) / TRIG_MAX_ANGLE) + center.x,
+    .y = (int16_t)(-cos_lookup(TRIG_MAX_ANGLE * lAngle/360) * (int32_t)(s_radius - ARROW_MARGIN - ARROW_IN) / TRIG_MAX_ANGLE) + center.y,
   };
 
   GPoint r_point = (GPoint) {
-    .x = (int16_t)(sin_lookup(rAngle) * (int32_t)(s_radius - ARROW_MARGIN) / 360) + center.x,
-    .y = (int16_t)(-cos_lookup(rAngle) * (int32_t)(s_radius - ARROW_MARGIN) / 360) + center.y,
+    .x = (int16_t)(sin_lookup(TRIG_MAX_ANGLE * rAngle/360) * (int32_t)(s_radius - ARROW_MARGIN - ARROW_IN) / TRIG_MAX_ANGLE) + center.x,
+    .y = (int16_t)(-cos_lookup(TRIG_MAX_ANGLE * rAngle/360) * (int32_t)(s_radius - ARROW_MARGIN - ARROW_IN) / TRIG_MAX_ANGLE) + center.y,
   };
-
 
 
   graphics_context_set_fill_color(ctx, GColorBlack);
